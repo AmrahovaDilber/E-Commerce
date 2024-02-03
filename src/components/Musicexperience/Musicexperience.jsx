633933904cf0d67dbfe258a9a1d1b6ab496de64f
AmrahovaDilber <amrahovad911@gmail.com>
2024-02-03 12:07:02 +0400

    changes

--- src/components/Musicexperience/Musicexperience.jsx
+++ src/components/Musicexperience/Musicexperience.jsx
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Musicexperience/Musicexperience.css";
 import music from "../../images/music.png";
 import { Link } from "react-router-dom";
+
 export default function Music() {
+  const calculateTimeLeft = () => {
+    const now = new Date();
+const targetDate = new Date("2024-12-31T23:59:59");
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      return { hours: 0, minutes: 0, seconds: 0 };
+    }
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return { hours, minutes, seconds };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="music">
       <div className="musiccontainer">
         <div className="left">
           <h1>Enhance Your Music Experience</h1>
           <div className="buttons">
+       
             <button>
-              <span>23</span>
-              <br></br> Hours
+              <span>{timeLeft.hours}</span>
+              <br /> Hours
             </button>
             <button>
-              <span> 05</span>
-              <br></br> Days
+              <span>{timeLeft.minutes}</span>
+              <br /> Minutes
             </button>
             <button>
-              <span> 59</span>
-              <br></br> Minutes
-            </button>
-            <button>
-              <span> 35</span>
-              <br></br> Seconds
+              <span>{timeLeft.seconds}</span>
+              <br /> Seconds
             </button>
           </div>
           <Link to="/products">
@@ -32,7 +59,7 @@ export default function Music() {
           </Link>
         </div>
         <div className="right">
-          <img src={music} alt="img"></img>
+          <img src={music} alt="img" />
         </div>
       </div>
     </div>
